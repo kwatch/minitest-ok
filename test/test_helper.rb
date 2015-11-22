1a6cf13b8b3178010a99369783b38bcecb29cd8e
makoto kuwata <kwa@kuwata-lab.com>
2015-11-22 22:11:31 +0900

    update 'test_helper.rb'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,12 @@
+# -*- coding: utf-8 -*-
+
+###
+### $Release: 0.0.0 $
+### $Copyright: copyright(c) 2015 kuwata-lab.com all rights reserved $
+### $License: MIT License $
+###
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require 'minitest/autorun'
 require 'minitest/ok'
-
